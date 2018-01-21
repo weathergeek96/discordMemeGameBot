01a8993d0c6494555119f48d5edb0c88e9d76816
Caleb Chapman <cchapman0069@kctcs.edu>
2018-01-21 10:01:15 -0500

    Update MemeBot.java

--- src/memebot/MemeBot.java
+++ src/memebot/MemeBot.java
@@ -15,7 +15,7 @@ public class MemeBot {
    
    public static void main(String[] arguments) throws Exception {
        
-       JDA jda = new JDABuilder(AccountType.BOT).setToken("MzU0NzUyNTkwMjc1MjgwODk3.DJC0pA.9FGlH-EZTShKwnfGGfMcyonmy8I").buildAsync();
+       JDA jda = new JDABuilder(AccountType.BOT).setToken("TOKEN_HERE").buildAsync();
        jda.setAutoReconnect(true);
        jda.addEventListener(new MyListener());
        MyListener.load();
